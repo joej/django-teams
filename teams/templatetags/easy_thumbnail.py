1d5f8d571c7f2748e03f79991e1f8c55eb1af4cd
Charly Wilhelm <charly.wilhelm@gmail.com>
2013-01-24 21:11:32 +0100

    delete unused stuff

--- teams/templatetags/easy_thumbnail.py
+++
@@ -1,8 +0,0 @@
-from django.template import Library
-from easy_thumbnails.templatetags import thumbnail
-
-register = Library()    
-
-@register.tag(name="easy_thumbnail")
-def easy_thumbnail(parser, token):
-    return thumbnail.thumbnail(parser, token)
