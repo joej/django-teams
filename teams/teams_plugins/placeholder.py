81013301829b7c5c50e07ab040d7363d4df3a448
Charly Wilhelm <charly.wilhelm@gmail.com>
2013-07-09 23:20:35 +0200

    update for use with django-cms 3.0

--- teams/teams_plugins/placeholder.py
+++ teams/teams_plugins/placeholder.py
@@ -45,10 +45,27 @@ class SquadPlaceholder(SquadBase):
 
     content_placeholder = PlaceholderField('content')
 
+    def acquire_context(self):
+        """
+        Inspect the stack to acquire the current context used,
+        to render the placeholder. I'm really sorry for this,
+        but if you have a better way, you are welcome !
+        """
+        frame = None
+        try:
+            for f in inspect.stack()[1:]:
+                frame = f[0]
+                args, varargs, keywords, alocals = inspect.getargvalues(frame)
+                if 'context' in args:
+                    return alocals['context']
+        finally:
+            del frame
+
     @property
     def html_content(self):
         """No additional formatting is necessary"""
-        return self.content
+        context = self.acquire_context()
+        return render_placeholder(self.content_placeholder, context)
 
     class Meta:
         """SquadPlaceholder's Meta"""
